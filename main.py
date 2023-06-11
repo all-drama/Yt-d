15bd5b31e5fe6cc1cbb5cc295fd93fea6fd3ceaf
all-drama <90520716+all-drama@users.noreply.github.com>
2023-06-11 12:05:11 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import random
+import streamlit as st
+from downloader import video, playlist, channel
+
+# Function for some random animations
+def random_celeb():
+    return random.choice([st.balloons()])
+
+# Integration of all the downloader functions
+def main():
+    st.title("YouTube Downloader")
+    url = st.text_input(label="Paste your YouTube URL")
+    if st.button("Download"):
+        if url:
+            try:
+                with st.spinner("Loading..."):
+                    if 'playlist' in url:
+                        playlist(url)
+                    elif 'channel' in url:
+                        channel(url)
+                    else:
+                        video(url)
+                random_celeb()
+            except Exception as e:
+                st.error(e)
+
+if __name__ == "__main__":
+    main()
+    
